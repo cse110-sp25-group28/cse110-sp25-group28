4d604c6d83a9c1301bdd258ca0b0d9358af11783
L0oter1 <147905421+L0oter1@users.noreply.github.com>
2025-05-10 20:57:45 -0700

    Transfer files to main project
    Previous files were in a seperate repository, I've moved it here

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import jest from 'eslint-plugin-jest';
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  {
+    ignores: ['docs/styles/**', 'docs/scripts/**', 'dist/**', 'coverage/**'],
+  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { files: ['**/*.test.js', '**/__tests__/**/*.js'], plugins: { jest }, languageOptions: { globals: jest.environments.globals.globals } },
+]);
