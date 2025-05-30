7c2745eb1dea7862657eea46852496d2ac3ba39e
jeremysu99 <jeremysu99@gmail.com>
2025-05-30 14:28:03 -0700

    updated for tests

--- tests/puppeteer-demo.test.js
+++ tests/puppeteer-demo.test.js
@@ -7,7 +7,7 @@ describe('Basic user flow for Website', () => {
   it('Clicking Create New Deck redirects to create-deck.html', async () => {
     await page.click('.create-deck');
     const url = page.url();
-    expect(url).toContain('create-deck.html');
+    expect(url).toContain('source/create-deck.html');
   }, 10000);
 
   it('Clicking View Deck redirects to index.html', async () => {
