7b0058d163bbfd1e991b68fb1994c65bf3792578
L0oter1 <147905421+L0oter1@users.noreply.github.com>
2025-06-01 18:55:57 -0700

    Update main-page.test.js

--- tests/main-page.test.js
+++ tests/main-page.test.js
@@ -3,11 +3,7 @@ describe('create deck test', () => {
     await page.goto('https://cse110-sp25-group28.github.io/cse110-sp25-group28', {
     });
 
-    // Clear localStorage in case a previous test run interfered
-    await page.evaluate(() => {
-      localStorage.clear();
-    });
-
+ 
     // Reload the page so localStorage changes take effect
     await page.reload({ waitUntil: 'domcontentloaded' });
   });
@@ -19,4 +15,3 @@ describe('create deck test', () => {
     expect(numWorkoutSets).toBe(4);
   }, 10000);
 });
-  
