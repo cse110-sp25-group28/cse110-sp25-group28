2e8601e4991508600be7e8a3c24fded25a8abfb1
Ulziikhutag Davaasuren <ulziihutag2004@gmail.com>
2025-06-06 21:05:42 -0700

    added comments

--- source/assets/scripts/header.js
+++ source/assets/scripts/header.js
@@ -1,3 +1,4 @@
+//A click event-listener for logo-section to go to main page.
 window.addEventListener("DOMContentLoaded", () => {
     const logoSection = document.querySelector(".logo-section");
     if (!logoSection) return;
