1b3349e25e78f9b3d0043940e7ed7e52d17b9dbc
jeremysu99 <jeremysu99@gmail.com>
2025-06-07 00:49:01 -0700

    Revert "Revert "fixed errors""
    This reverts commit 253ae8b0526a80778343629e2812214164e268cb.
    
    revert#

--- source/assets/scripts/shuffle.js
+++ source/assets/scripts/shuffle.js
@@ -7,7 +7,7 @@
  * @returns {Array} New array with the elements shuffled
  * @throws {TypeError} If the input is not array
  */
-function shuffle(cards) {
+export function shuffle(cards) {
     if (!Array.isArray(cards)) {
         throw new TypeError('Cards must be an array');
     }
@@ -15,11 +15,7 @@ function shuffle(cards) {
     const result = cards.slice();
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [result[i], result[j]] = [result[j],result[i]];
+        [result[i], result[j]] = [result[j], result[i]];
     }
     return result;
 }
-
-module.exports = {
-    shuffle,
-};
